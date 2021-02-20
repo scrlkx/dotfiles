dcf654f14f01a961defa2cc519be5acfa0b9cabc
Daniel Freitas <daniel1208x@gmail.com>
2021-02-20 15:01:09 -0300

    better readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dotfiles
-@scrlkx dotfiles.
+
+Set of my personal dotfiles.
+
+## What's inside
+
+There are several cool things, with settings for git, hyper, zsh and more.
+
+## Usage
+
+1. `cd $HOME && git clone git@github.com:scrlkx/dotfiles.git`
+2. `cd $HOME/dotfiles`
+3. `sudo chmod +x install.sh`
+4. `./install.sh`
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -s -f  ${BASEDIR}/aliases ~/.aliases
+ln -s -f ${BASEDIR}/aliases ~/.aliases
 ln -s -f ${BASEDIR}/gitconfig ~/.gitconfig
 ln -s -f ${BASEDIR}/functions ~/.functions
-ln -s -f  ${BASEDIR}/hyper.js ~/.hyper.js
-ln -s -f  ${BASEDIR}/localrc ~/.localrc
-ln -s -f  ${BASEDIR}/zshrc ~/.zshrc
+ln -s -f ${BASEDIR}/hyper.js ~/.hyper.js
+ln -s -f ${BASEDIR}/localrc ~/.localrc
+ln -s -f ${BASEDIR}/zshrc ~/.zshrc
