87e186690144ef66241802c2b6da842e14b5951f
Daniel Freitas <daniel1208x@gmail.com>
2021-02-27 12:21:28 -0300

    sublime settings

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ ln -s -f ${BASEDIR}/gitconfig ~/.gitconfig
 ln -s -f ${BASEDIR}/functions ~/.functions
 ln -s -f ${BASEDIR}/hyper.js ~/.hyper.js
 ln -s -f ${BASEDIR}/localrc ~/.localrc
+ln -s -f ${BASEDIR}/sublime ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 ln -s -f ${BASEDIR}/zshrc ~/.zshrc
